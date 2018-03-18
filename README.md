b02d230b05806d0845f06b8f8ec9f0809e929b41
rrama <ben_durrans@hotmail.co.uk>
2018-03-18 00:40:01 +0000

    Add script to test all example and bad input data
    Add bad input examples.
    Add move valid input examples.
    Remove white space at end of lines in the input files.
    Make run.sh executable from any location.
    Rename "example_data/07.txt" to 'single_node_01.txt'

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # DPC-Ex2
 UoB CS DPC Assessment 2 - Distribution
+
+## Compiling
+
+The code has been written and tested for Erlang 20.2.  
+To compile the code, run from this directory:
+```bash
+erlc *.erl
+```
+
+## Running
+
+To run the code, execute from this directory:
+```bash
+erl -noshell -run tarry main -run init stop
+```
+and pipe into `stdin` the input data.
+
+The script [run.sh](run.sh) has been provided which takes an input file as an aregument and pipes it into the program for you.
+
+## Testing
+
+To test the code with the well-formed input data in [example_data/](example_data/) and the malformed input data in [bad_inputs/](bad_inputs/) run the testing script [test_all.sh](test_all.sh).
--- run.sh
+++ run.sh
@@ -1,3 +1,63 @@
 #!/bin/bash
 
-cat "${1}" | erl -noshell -run tarry main -run init stop
+# --- Constant definitions. ---
+
+# x is empty.
+x=''
+# The name of this script.
+scriptName='run.sh'
+
+# --- --- ---
+
+
+# --- Function definitions. ---
+
+function error_exit
+{
+#   ----------------------------------------------------------------
+#   Print error message and exit.
+#       Accepts 2 argument:
+#           string - optional - error message
+#           integer - optional - the exit code
+#   ----------------------------------------------------------------
+    echo "${scriptName}: ${1:-"Unknown Error"}" 1>&2
+    exit ${2:-"1"}
+}
+
+# --- --- ---
+
+
+# --- Check arguments. ---
+
+if [ -z "${1+x}" ]; then
+    error_exit "Usage: ./${scriptName} input_data_file" 25
+fi
+
+# --- --- ---
+
+
+# --- Determine the location of this script. ---
+
+if [ ! -z "${0+x}" ]\
+&& [ -e "${0}" ]\
+&& [[ "${0}" == *"${scriptName}" ]]\
+&& [ "$( basename "${0}" )" = "${scriptName}" ]; then
+    scriptLocation="$( dirname "${0}" )"
+elif [ -e "$( pwd )/${scriptName}" ]; then
+    scriptLocation="$( pwd )"
+elif [ -e "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/${scriptName}" ]; then
+    scriptLocation="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+else
+    error_exit 'Can not determine the location of this script...\nPlease run this script from the same folder as the script.' 26
+fi
+
+# --- --- ---
+
+
+# --- Run the scripts. ---
+
+fileContent="$( cat "${1}" )"
+cd "${scriptLocation}"
+echo "${fileContent}" | erl -noshell -run tarry main -run init stop
+
+# --- --- ---
